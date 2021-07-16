4d4e35abe6c6156e52116d357d8934bed7d6c75e
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-07-16 00:03:06 -0300

    Create condicional.php

---
+++ condicional.php
@@ -0,0 +1,14 @@
+<?php
+
+// Condiconais IF e ELSE
+
+
+$idade = 18;
+
+echo "<h1>Site adulto</h1>";
+
+if($idade >=18 ) {
+    echo "Você é maior de idade! Possui {$idade}, anos!!";
+}else{
+    echo"Você é menor de idade, tem apenas {$idade}, é jovem ainda :c";
+};
