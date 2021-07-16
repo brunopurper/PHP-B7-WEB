1f5319262b3ff7ea86956dda4ce4a79b79e2ba85
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-07-16 00:11:38 -0300

    Update condicional.php

--- condicional.php
+++ condicional.php
@@ -1,14 +1,24 @@
+
+<h1>Site adulto</h1>
+<h2>Digite sua idade para entrar</h2>
+<form action="condicional.php" method="post"> 
+    Digite sua idade para acessar o site:
+    <input type="text" name="form_idade" > </br> <br>
+    <input type="submit">
+</form>
+
 <?php
 
 // Condiconais IF e ELSE
 
+$idade = $_POST['form_idade'] ?? null;
 
-$idade = 18;
 
-echo "<h1>Site adulto</h1>";
 
-if($idade >=18 ) {
+if($idade >= 18 ) {
     echo "Você é maior de idade! Possui {$idade}, anos!!";
 }else{
     echo"Você é menor de idade, tem apenas {$idade}, é jovem ainda :c";
 };
+
+?>
