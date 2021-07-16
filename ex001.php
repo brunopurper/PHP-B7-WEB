fd5fff551a7b6025341c151671aab6fc9ee1f1d2
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-07-15 23:56:57 -0300

    Update ex001.php

--- ex001.php
+++ ex001.php
@@ -1,6 +1,19 @@
 <?php
 
-$lista = {
+$lista = [
     "nome" => "Bruno",
+    "idade" => "90",
+    "atributos" => [
+        "forca" => "60", 
+        "agilidade" => "90", 
+        "destreza" => "50", 
+    ],
     
-}
+    'vida' => 2000,
+    'mana' => 929
+
+];
+
+echo "NOME: " . $lista['nome'] . "</br>";
+echo "FORÇA: " . $lista['atributos']['forca'] . "</br>";
+echo "VIDA: " . $lista['vida'] . "</br>";
